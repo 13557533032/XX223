8b26709cb749b8aac01aa6ddd2dd5a69c9af6676
罗心铭 <318658807@qq.com>
2020-04-02 16:12:40 +0800

    添加加法

--- text/text.lua
+++ text/text.lua
@@ -4,4 +4,10 @@ function textClass:init()
     local a = 6
     local c = 6
     local d = 6
+
+    print(self:sun(a,d,c))
+end
+
+function textClass:sun(arg1, arg2, arg3)
+    return tonumber(arg1..arg2..arg3)
 end
